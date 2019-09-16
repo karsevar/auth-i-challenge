c2f0da159cd96c75850d669a1fee124574fd4eae
masonkarsevar <masonkarsevar@gmail.com>
2019-09-16 14:47:30 -0700

    client application almost complete

--- login-signup/src/App.js
+++ login-signup/src/App.js
@@ -1,33 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import './App.css';
 import {Route, Link} from 'react-router-dom';
 import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
+
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
   const loginHandler = (creds, e) => {
     e.preventDefault();
     axios
       .post('http://localhost:4000/api/login', creds)
       .then(res => {
-        console.log(res);
+        console.log(res.data.message);
+        setError('');
+        setSuccess(res.data.message);
       })
       .catch(error => {
         console.log(error);
+        setSuccess('');
+        setError('Invalidate User')
       })
   }
-  
+
+  const signupHandler = (creds, e) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:4000/api/register', creds)
+      .then(res => {
+        console.log(res);
+        setError('');
+        setSuccess(`${res.data.username} has been registered`);
+      })
+      .catch(error => {
+        console.log(error)
+        setSuccess('');
+        setError('Invalidate Post')
+      })
+  }
+
   return (
     <div className="App">
       <div className='app-navigation'>
         <Link to='/login'>Login</Link>
+        <Link to='/signup'>Signup</Link>
       </div>
       <Route path='/login' render={props => {
-        return <Login {...props} loginHandler={loginHandler} /> 
+        return <Login {...props} loginHandler={loginHandler} error={error} success={success} /> 
       }} />
-      {/* <Route path='/signup' render={props => {
-        return <Signup {...props} signupHandler={signupHandler} /> 
-      }} /> */}
+      <Route path='/signup' render={props => {
+        return <Signup {...props} signupHandler={signupHandler} error={error} success={success} /> 
+      }} />
     </div>
   );
 }
--- login-signup/src/components/Login.js
+++ login-signup/src/components/Login.js
@@ -1,4 +1,32 @@
 import React, {useState} from 'react';
+import {Button, Form, Container} from 'semantic-ui-react';
+import styled from 'styled-components';
+
+
+const SignupForm = styled.div`
+    width: 300px;
+    margin: 0 auto;
+`;
+
+export const ErrorParagraph = styled.p`
+    padding: 16px;
+    background-color: #ffb7b7;
+    color: red;
+    border: solid 1px #f39595;
+    border-radius: 4px;
+    max-width: 700px;
+    margin: 16px auto;
+`;
+
+export const SuccessParagraph = styled.p`
+    padding: 16px;
+    background-color: lightgreen;
+    color: darkgreen;
+    border: solid 1px darkgreen;
+    border-radius: 4px;
+    max-width: 700px;
+    margin: 16px auto;
+`;
 
 const Login = (props) => {
     const [creds, setCreds] = useState({username: '', password: ''});
@@ -9,25 +37,37 @@ const Login = (props) => {
     };
 
     return (
-        <form onSubmit={e => props.loginHandler(creds, e)}>
-            <label>Username</label>
-            <input 
-                name='username'
-                type='text'
-                value={creds.username}
-                onChange={changeHandler}
-                placeholder='Username'
-            />
-            <label>password</label>
-            <input 
-                name='password'
-                type='password'
-                value={creds.password}
-                onChange={changeHandler}
-                placeholder="Password"
-            />
-            <button>Submit!</button>
-        </form>
+        <>
+            <SignupForm>
+                <Container textAlign='center'>
+                        <Form onSubmit={e => props.loginHandler(creds, e)}>
+                            <Form.Field>
+                                <label>Username</label>
+                                <input
+                                    name='username'
+                                    type='text'
+                                    value={creds.username}
+                                    onChange={changeHandler}
+                                    placeholder='Username'
+                                />
+                            </Form.Field> 
+                            <Form.Field>
+                                <label>Password</label>
+                                <input
+                                    name='password'
+                                    type='password'
+                                    value={creds.password}
+                                    onChange={changeHandler}
+                                    placeholder="Password"
+                                />
+                            </Form.Field>
+                            <Button>Login!</Button>
+                        </Form>
+                </Container>
+            </SignupForm>
+            {props.success && <SuccessParagraph>{props.success}</SuccessParagraph>}
+            {props.error && <ErrorParagraph>{props.error}</ErrorParagraph>}
+        </>
     )
 }
 
